6e4a57201e11e344792b1a8858e950022409bb0e
Mr.zhou <zzmy1995@gmail.com>
2026-06-04 13:51:37 +0800

    fix(edgeone): 调整 Pages 函数入口导出

--- cloud-functions/config.ts
+++ cloud-functions/config.ts
@@ -1,3 +1,5 @@
 import { handleEdgeOnePagesRequest } from '../src/edgeone/handler';
 
-export const onRequest = handleEdgeOnePagesRequest;
+export default function onRequest(context: Parameters<typeof handleEdgeOnePagesRequest>[0]): Promise<Response> {
+  return handleEdgeOnePagesRequest(context);
+}
